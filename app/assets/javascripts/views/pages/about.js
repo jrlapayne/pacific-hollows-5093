9c3f53218cdef0e06117663447c9bb12208a5801
jrlapayne <jrlapayne@gmail.com>
2012-10-25 19:05:43 -0500

    7:05pmOct25

--- app/assets/javascripts/views/pages/about.js
+++ app/assets/javascripts/views/pages/about.js
@@ -19,22 +19,25 @@ Gangnam.Views.PagesAbout = Backbone.View.extend({
 		$(this.el).html(this.template());
 		setTimeout(function() {
 			self.openFirst();
-		}, 500)
+		}, 200);
 		return this;
 	},
 	
 	openBox: function(event) {
 		var new_ele = $(event.target).closest('.comparison');
 		var old_ele;
-		var elements = $('#about').children().get();
+		var elements = $(this.el).children().get();
 		
+		if ($(new_ele).hasClass('open')) {
+			return;
+		}
 		for (i = 0; i < elements.length; i++) {
 			if ($(elements[i]).hasClass('open')) {
 				old_ele = elements[i];
 				$(old_ele).removeClass('open');
 				setTimeout(function() {
 					$(old_ele).find('.panels').css('display', 'none');
-				}, 1000)
+				}, 1000);
 			}
 		}
 		$(new_ele).addClass('open');
@@ -42,19 +45,36 @@ Gangnam.Views.PagesAbout = Backbone.View.extend({
 		$(old_ele).find('.panels').animate({
 			height: "0px"
 		}, 1000);
-		
+		$(old_ele).find('#arrows').find('.down').animate({
+			opacity: "0"
+		}, 1000);
+		$(old_ele).find('#arrows').find('.right').animate({
+			opacity: "1"
+		}, 1000);
 		$(new_ele).find('.panels').animate({
 			height: "130px"
+		}, 1000);
+		$(new_ele).find('#arrows').find('.down').animate({
+			opacity: "1"
+		}, 1000);
+		$(new_ele).find('#arrows').find('.right').animate({
+			opacity: "0"
 		}, 1000);
 	},
 	
 	openFirst: function() {
-		var element = $('#about').children().get()[0];
+		var element = $(this.el).children().get()[0];
 		
 		$(element).addClass('open');
 		$(element).find('.panels').css('display', 'block');
 		$(element).find('.panels').animate({
 			height: "130px"
+		}, 1000);
+		$(element).find('#arrows').find('.down').animate({
+			opacity: "1"
+		}, 1000);
+		$(element).find('#arrows').find('.right').animate({
+			opacity: "0"
 		}, 1000);
 	},
 	
